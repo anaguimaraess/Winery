437568f4a469dc424440cb78d0b3dfb3ad5813ba
Ana Carolina Guimarães <a.carolina1903@gmail.com>
2023-08-17 20:10:30 -0300

    feat: subindo dependencias e inicio da estrutura

---
+++ src/main/java/br/com/ecommerce/winery/WineryApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecommerce.winery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineryApplication.class, args);
+	}
+
+}
