3d2820a73e99055a50adda90f3ebdd4a7671fc90
Ana Carolina Guimarães <a.carolina1903@gmail.com>
2023-08-18 14:30:13 -0300

    feat: adicionando security e impl logs

---
+++ src/main/java/br/com/ecommerce/winery/utils/SecurityConfig.java
@@ -0,0 +1,34 @@
+package br.com.ecommerce.winery.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/admin/cadastrar").permitAll() // Permite acesso não autenticado ao endpoint de cadastro
+                .antMatchers("/h2-console/**").permitAll() // Permite acesso ao console do H2
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+                .headers().frameOptions().sameOrigin(); // Permite o uso do frame do H2 no mesmo domínio
+    }
+
+}
+
+
