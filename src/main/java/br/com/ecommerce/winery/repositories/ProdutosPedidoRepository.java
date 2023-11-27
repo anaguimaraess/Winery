894ad8f561c1522bec4e5d65ae9a8206e6236477
Ana Carolina Guimarães <a.carolina1903@gmail.com>
2023-11-26 21:44:25 -0300

    deletando arquivo nao utilizado

--- src/main/java/br/com/ecommerce/winery/repositories/ProdutosPedidoRepository.java
+++
@@ -1,7 +0,0 @@
-//package br.com.ecommerce.winery.repositories;
-//
-//import br.com.ecommerce.winery.models.pedido.ProdutosPedido;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface ProdutosPedidoRepository extends JpaRepository<ProdutosPedido, Integer> {
-//}
